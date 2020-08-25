69351f16c3deef837871db70cec9567f53348b49
Josimar16 <josimarjr479@gmail.com>
2020-08-24 22:23:32 -0300

    colocando funções de deletar, inserir e puxar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect(() => {
+    api({url: '/projects', params: {title: 'Novo repo 2'}, method: 'GET'}).then(response => setRepositories(response.data)).catch(error => console.log(error))
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const repository = await api.post('project', { title: 'Novo repo', owner: 'repo' })
+    setRepositories([...repositories, repository.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`project/${id}`)
+    const index = await repositories.findIndex(repository => repository.id === id)
+    repositories.slice(index, 1)
+    return repositories
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(
+            repository => 
+              <li key={repository}>
+                {repository.title}
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
